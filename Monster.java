ea29bba6b7360a96a17834d43ac26245854f3fec
Quan <quan_ho35@yahoo.de>
2019-05-16 12:55:55 +0200

    Monster

---
+++ Monster.java
@@ -0,0 +1,11 @@
+
+public abstract class Monster
+{
+    
+    public abstract void datenAusgeben();
+    public abstract String nameGeben();
+    public abstract String eigenschaftGeben();
+    public abstract String merkmalGeben();
+    public abstract void speichern();
+
+}
---
+++ Ryuu.java
@@ -0,0 +1,25 @@
+public class Ryuu extends Monster
+{
+    private String name;
+    private String eigenschaft;
+    private String merkmal;
+    private int erfahrung;
+    private int istHungrig;
+    private int istSchmutzig;
+    private int istGelangweilt;
+    private int istMüde;
+    public Ryuu(String name)
+    {
+        this.name = name;
+        merkmal = "stolz";
+        int a = getrandomint(3);
+        if(a == 1)
+        {
+            eigenschaft = 
+        }
+        else if()
+        else
+        
+    }
+}
+
