aa1cb243d9a3d3df31c9a05a8a91a9e767b5942e
Jonas <the.dj.2702@gmail.com>
2019-06-29 19:01:36 +0200

    +schlüssel

--- Spiel.java
+++ Spiel.java
@@ -1,18 +1,46 @@
-import java.util.Scanner;
+import java.util.*;
 public class Spiel
 {
     private RaumListe RListe;
     private Monster Mst;
     private Raum Standort;
-
+    private String s_name;
+    private String s_spezies;
+    private Scanner s;
     public Spiel()
     {
         System.out.print('\f');
-        Monster_Erstellen();
-
+        s = new Scanner(System.in);
+        start();
     }
 
-    public void Monster_Erstellen()
+    public void start()
+    {
+        String schluessel;
+        String eingabe;
+        
+        System.out.println("Willkommen bei Project Grapefruit. Um fortzufahren, müssen wir wissen, ob du bereits einen Schlüssel besitzt. Wenn ja, dann schreibe bitte ja, ansonsten nein.");
+        eingabe = s.next();
+        
+        if(eingabe.equals("ja"))
+        {
+            System.out.println("Bitte gebe nun deinen Schlüssel ein.");
+            schluessel = s.next();
+            schluesselentschluesseln(schluessel);
+            System.out.println("Vielen Dank, dein Zustand wurde wiederhergestellt, viel Spaß!");
+        }
+        
+        else if(eingabe.equals("nein"))
+        {
+            Monster_Erstellen();
+        }
+        else
+        {
+            System.out.println("Ungültige Eingabe. Bitte überprüfe deine Eingabe, das Setup wird neugestartet.");
+        }
+    }
+    
+    private void Monster_Erstellen()
     {
         System.out.println("Wähle eine Spezies!");
         System.out.println("----");
@@ -30,7 +58,9 @@ public class Spiel
         {            
             System.out.println("Glückwunsch, ein Ryuu ist geschlüpft.");
             System.out.println("Wie soll es heißen? ");
-            String name = s.next();        
+            String name = s.next();
+            s_name = name;
+            s_spezies = "Ryuu";
             Mst = new Ryuu(name);
             System.out.println("----");
         }
@@ -39,6 +69,8 @@ public class Spiel
             System.out.println("Glückwunsch, ein Robu ist geschlüpft.");
             System.out.println("Wie soll es heißen? ");
             String name = s.next();
+            s_name = name;
+            s_spezies = "Robu";
             Mst = new Robu(name);
             System.out.println("----");
         }
@@ -49,5 +81,27 @@ public class Spiel
         }
     }
     
+    public void speichern()
+    {
+        System.out.println("Dein Spielstand wird nun gespeichert. Dazu wird dir ein Schlüssel ausgegeben, welchen du bitte auf deinem Rechner speicherst.");
+        System.out.println(schluesselerstellen());
+    }
     
+    private String schluesselerstellen()
+    {
+        String name = Base64.getEncoder().withoutPadding().encodeToString(s_name.getBytes());
+        String spezies = Base64.getEncoder().withoutPadding().encodeToString(s_spezies.getBytes());
+        return name+","+spezies;
+    }
+    
+    private void schluesselentschluesseln(String schlüssel)
+    {
+        String[] teile = schlüssel.split(",");
+        String teil1 = teile[0];
+        String teil2 = teile[1];
+        String name = new String(Base64.getDecoder().decode(teil1.getBytes()));
+        String spezies = new String(Base64.getDecoder().decode(teil2.getBytes()));
+        s_name = name;
+        s_spezies = spezies;
+    }
 }
